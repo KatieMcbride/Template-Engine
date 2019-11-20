32add99ac1eb8240b8e32031caea2386b8036a93
Katie McBride <mcbride.katieb@gmail.com>
2019-11-19 19:52:29 -0600

    start employee function

--- node.js
+++ node.js
@@ -2,4 +2,19 @@
 const inquirer = require("inquirer");
 const test = require("jest");
 
-// 
+// Write Employee function
+
+class Employee {
+    constructor(name,id,email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+        return this.name
+    }
+}
+
+
+module.exports = Employee;
