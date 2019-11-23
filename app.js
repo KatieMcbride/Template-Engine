879317c3feffcc9929591e0253de8712dd8e5231
Katie McBride <mcbride.katieb@gmail.com>
2019-11-22 21:47:13 -0600

    css and working html output

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const path = require("path");
 const fs = require("fs");
-const outputPath = path.resolve(__dirname, "output", "team.html");
+// const outputPath = path.resolve(__dirname, "output", "team.html");
 const teamMembers = [];
 const idArray = [];
 
@@ -166,11 +166,54 @@ function buildTeam() {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
             <title>Employee</title>
         </head>
         <body>
-            <div> Employee name: ${teamMembers[0].name}</div>
-        </body>
+        <div class="jumbotron">
+            <h1 class= "text-center">Team Members</h1>
+        </div>
+        <div class="row d-flex">
+        <div class="col-3 card m-5">
+            <div>
+                <div class="card-header text-center text-white bg-primary font-weight-bold>
+                Manager
+                </div>
+                <div> Employee name: ${teamMembers[0].name}</div>
+                <div> Employee iD: ${teamMembers[0].id}</div>
+                <div> Employee email: ${teamMembers[0].email}</div>
+                <div> Employee office Number: ${teamMembers[0].officeNumber}</div>
+            </div>
+        </div>
+        <div class="col-3 card m-5">
+            <div class="container">
+                <div>
+                <div class="card-header text-center text-white bg-success font-weight-bold">
+                 Engineer
+                </div>
+                <div> Employee name: ${teamMembers[1].name}</div>
+                <div> Employee iD: ${teamMembers[1].id}</div>
+                <div> Employee email: ${teamMembers[1].email}</div>
+                <div> Employee github name: ${teamMembers[1].github}</div>
+            </div>
+            </div>
+        </div>
+        <div class="col-3 card m-5">
+            <div class="container">
+                <div>
+                    <div class="card-header text-center text-white bg-warning font-weight-bold">
+                    Intern
+                    </div>
+                    <div> Employee name: ${teamMembers[2].name}</div>
+                    <div> Employee iD: ${teamMembers[2].id}</div>
+                    <div> Employee email: ${teamMembers[2].email}</div>
+                    <div> Employee school: ${teamMembers[2].school}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+            
+    </body>
     </html>`
 
     return fs.writeFileSync("team.html", htmlBuild , "utf-8")
